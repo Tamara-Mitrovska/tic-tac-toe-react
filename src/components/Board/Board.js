4f5458ed40d1ed2c537b1a416683ef6c29cf02da
Tamara Mitrovska <tamaram@mit.edu>
2024-11-20 21:22:58 -0500

    add state to board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,35 +1,94 @@
-import { useState } from "react";
+import React from "react";
 import Square from "../Square/Square";
 import "./Board.css";
 
 const N = 3;
 
-export default function Board() {
-  const [values, setValues] = useState([].constructor(N * N).fill(""));
-  const [turn, setTurn] = useState(false);
-  let onSquareClick = (i) => {
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: [].constructor(N * N).fill(""),
+      turn: false,
+      winner: null,
+      numClicks: 0,
+    };
+  }
+  onSquareClick(i) {
     // clicking on a filled square doesn't change anything
-    if (values[i] === "") {
-      const newValues = values.slice();
-      newValues[i] = turn ? "X" : "O";
-      setValues(newValues);
-      setTurn(!turn);
+    if (
+      this.state.numClicks < N * N &&
+      !this.state.winner &&
+      this.state.values[i] === ""
+    ) {
+      const newValues = this.state.values.slice();
+      newValues[i] = this.state.turn ? "X" : "O";
+      const newWinner = this.checkWinner(i, newValues);
+      this.setState({
+        values: newValues,
+        turn: !this.state.turn,
+        winner: newWinner,
+        numClicks: this.state.numClicks + 1,
+      });
     }
-  };
+  }
 
-  return (
-    <div className="board">
-      {[1, 2, 3].map((_, row) => (
-        <div className="board__row" key={row}>
-          {[1, 2, 3].map((_, col) => (
-            <Square
-              key={row * 3 + col}
-              value={values[row * 3 + col]}
-              onSquareClick={() => onSquareClick(row * 3 + col)}
-            />
-          ))}
+  checkWinner(square, values) {
+    // check row
+    let val = values[square];
+    let row = Math.floor(square / N);
+    let col = square % N;
+    if (
+      values.filter((v, i) => Math.floor(i / N) === row && v !== val).length ===
+      0
+    ) {
+      return val;
+    }
+    // check column
+    if (values.filter((v, i) => i % N === col && v !== val).length === 0) {
+      return val;
+    }
+    // check left diagonal
+    if (square % N === Math.floor(square / N)) {
+      if (
+        values.filter((v, i) => i % N === Math.floor(i / N) && v !== val)
+          .length === 0
+      ) {
+        return val;
+      }
+    }
+    // check right diagonal
+    if ((square % N) + Math.floor(square / N) === N - 1) {
+      if (
+        values.filter(
+          (v, i) => (i % N) + Math.floor(i / N) === N - 1 && v !== val
+        ).length === 0
+      ) {
+        return val;
+      }
+    }
+    return this.state.winner;
+  }
+  render() {
+    return (
+      <div className="board">
+        <div>
+          {this.state.winner
+            ? `The winner is ${this.state.winner}`
+            : "No winner yet!"}
         </div>
-      ))}
-    </div>
-  );
+        {this.state.values.slice(0, N).map((_, row) => (
+          <div className="row" key={row}>
+            {this.state.values.slice(0, N).map((_, col) => (
+              <Square
+                key={row * 3 + col}
+                value={this.state.values[row * 3 + col]}
+                onSquareClick={() => this.onSquareClick(row * 3 + col)}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
